52918ce4b7d10c68cb75935e20a67f6e5c6af4b2
basti6670 <138782878+basti6670@users.noreply.github.com>
2024-02-13 13:28:09 +0100

    yy

--- src/website/websites/Boilerfunktion.js
+++ src/website/websites/Boilerfunktion.js
@@ -35,3 +35,27 @@ export function createCusomer(customerId,firstname, lastname ){
     let newCustomer = {customerid, firstname, lastname};
     customerData.push(newCustomer);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            displayCustomerData(JSON.parse(this.responseText));
+        }
+    };
+    xhttp.open("GET", "Customers.json", true);
+    xhttp.send();
+});
+ 
+function displayCustomerData(data) {
+    var htmlContent = "<table border='1'><tr><th>Vorname</th><th>Nachname</th><th>Kunden-ID</th><th>Erstregistrierung</th></tr>";
+    data.customers.forEach(function(customer) {
+        var firstname = customer.firstname;
+        var lastname = customer.lastname;
+        var customerID = customer["customer-id"];
+        var registrationDate = customer["registration-date"];
+        htmlContent += "<tr><td>" + firstname + "</td><td>" + lastname + "</td><td>" + customerID + "</td><td>" + registrationDate + "</td></tr>";
+    });
+    htmlContent += "</table>";
+    document.getElementById("customer-data").innerHTML = htmlContent;
+}
