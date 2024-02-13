1286d77878dc2c7896462318a59212888d743fd4
basti6670 <138782878+basti6670@users.noreply.github.com>
2024-02-13 10:38:44 +0100

    a

--- src/website/websites/Boilerfunktion.js
+++ src/website/websites/Boilerfunktion.js
@@ -2,24 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            displayCustomerData(this);
+            displayCustomerData(JSON.parse(this.responseText));
         }
     };
-    xhttp.open("GET", "Customers.xml", true);
+    xhttp.open("GET", "Customers.json", true);
     xhttp.send();
 });
 
-function displayCustomerData(xml) {
-    var xmlDoc = xml.responseXML;
-    var customers = xmlDoc.getElementsByTagName("customer");
+function displayCustomerData(data) {
     var htmlContent = "<table border='1'><tr><th>Vorname</th><th>Nachname</th><th>Kunden-ID</th><th>Erstregistrierung</th></tr>";
-    for (var i = 0; i < customers.length; i++) {
-        var firstname = customers[i].getElementsByTagName("firstname")[0].childNodes[0].nodeValue;
-        var lastname = customers[i].getElementsByTagName("lastname")[0].childNodes[0].nodeValue;
-        var customerID = customers[i].getElementsByTagName("customer-id")[0].childNodes[0].nodeValue;
-        var registrationDate = customers[i].getElementsByTagName("registration-date")[0].childNodes[0].nodeValue;
+    data.customers.forEach(function(customer) {
+        var firstname = customer.firstname;
+        var lastname = customer.lastname;
+        var customerID = customer["customer-id"];
+        var registrationDate = customer["registration-date"];
         htmlContent += "<tr><td>" + firstname + "</td><td>" + lastname + "</td><td>" + customerID + "</td><td>" + registrationDate + "</td></tr>";
-    }
+    });
     htmlContent += "</table>";
     document.getElementById("customer-data").innerHTML = htmlContent;
 }
