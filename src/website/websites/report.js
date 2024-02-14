f3f4b58d1568ba7f97b100ed9ca5f09e7ffd7f2a
basti6670 <138782878+basti6670@users.noreply.github.com>
2024-02-14 10:56:25 +0100

    a

--- src/website/websites/report.js
+++ src/website/websites/report.js
@@ -18,12 +18,7 @@ class Report {
         this.closeReason = closeReason;
         this.references = references || [];
     }
-
-    // Methoden zum Setzen von Eigenschaften
-    setDescription(description) {
-        this.description = description;
-    }
-
+    /*
     newReport() { //Kunde
         this.id = id + 1;
         //this.customerId = Lookup auf Kundentabelle
@@ -39,10 +34,10 @@ class Report {
         return this.comments, this.closeReason
     }
     archiveReport() { //Kunde
+*/
 
-    }
 
-    
+
     /*
     markAsFinished() { //Dev
         if (this.state != 'finished');
@@ -50,5 +45,57 @@ class Report {
         this.closedAt = new Date();
         //this.closeReason = Eingabe des Bearbeiters / Default = finished
         this.archiveReport();
-    } */
+    } 
+    */
+    
+}
+class ReportManager {
+    constructor() {
+        this.reports = [];
+    }
+
+    // Hinzufügen eines neuen Berichts
+    addReport(reportData) {
+        const newReport = new Report(reportData);
+        this.reports.push(newReport);
+        return newReport;
+    }
+
+    // Abrufen aller Berichte
+    getAllReports() {
+        return this.reports;
+    }
+}
+
+class Report {
+    constructor({ id, category, customerId, description, labels, owner, assignedTo, createdAt, editedAt, closedAt, state, priority, comments, closeReason, references }) {
+        this.id = id;
+        this.category = category;
+        this.customerId = customerId;
+        this.description = description;
+        this.labels = labels || [];
+        this.owner = owner;
+        this.assignedTo = assignedTo;
+        this.createdAt = createdAt;
+        this.editedAt = editedAt;
+        this.closedAt = closedAt;
+        this.state = state;
+        this.priority = priority;
+        this.comments = comments || [];
+        this.closeReason = closeReason;
+        this.references = references || [];
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    //bei beendigung
+    markAsFinished(reason) {
+        if (this.state !== 'finished') {
+            this.state = 'finished';
+            this.closedAt = new Date();
+            this.closeReason = reason;
+        }
+    }
 }
