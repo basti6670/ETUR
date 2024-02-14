a040e0e37961a7a94709800dedf4e00a60b8ce20
Nils-Reiber <nils.reiber@plan-b-gmbh.com>
2024-02-14 09:38:42 +0100

    report

--- src/website/websites/report.js
+++ src/website/websites/report.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Report {
     constructor(id, category, customerId, description, labels, owner, assignedTo, createdAt, editedAt, closedAt, state, priority, comments, closeReason, references) {
         this.id = id;
@@ -24,40 +23,7 @@ class Report {
     }
 
     // Weitere Methoden zum Setzen und Abrufen von Eigenschaften ...
-
-    // Exportieren der Klasse
-    module
-
 }
-=======
-const report = {
-    id: '2412',
-    category: "Feedback",
-    customerId: "1234",
-    description: "This is a description",
-    labels: ["label1", "label2"],
-    owner: "Product Manager",
-    assignedTo: "Jens Reiner",
-    createdAt: "2020-01-01:12:00:00",
-    editedAt: "2020-01-01:12:00:00",
-    closedAt: "2020-01-01:12:00:00",
-    state: "Open",
-    priority: 1,
-    comments: [
-      {
-        author: "Jens Reiner",
-        message: "This is a comment",
-        createdAt: "2020-01-01:12:00:00",
-        type: 'developer',
-      },
-    ],
-    closeReason: "This is a close reason",
-    references: [
-      {
-        type: "github",
-        url: "",
-        issueNumber: 1
-      }
-    ]
-  };
->>>>>>> 9af342cf57ad13fb12de7c64613ec1a6addb95e1
+
+// Exportieren der Klasse
+module.exports = Report;
